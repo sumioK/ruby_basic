5f7f002c7c868504cf1060f0ac7b83efe89da1f6
Sumio K <tap.miwo@gmail.com>
2022-10-31 22:07:12 +0900

    done

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,71 @@
-i = 1
+#while文を使用した場合
+# i = 1
+# while i < 32
+#   if i %3 == 0 && i %5 == 0
+#     print "FIzzBuzz,"
+#   elsif i %3 == 0
+#     print "Fizz,"
+#   elsif i %5 == 0
+#     print "Buzz,"
+#   else
+#     if i < 31
+#       print i
+#       print ","
+#     else
+#       print i
+#     end
+#   end
+#   i += 1
+# end
 
-while i < 32
-  if i %3 == 0 && i %5 == 0
-    print "FIzzBuzz,"
-  elsif i %3 == 0
-    print "Fizz,"
-  elsif i %5 == 0
-    print "Buzz,"
+#for文を使用
+
+# for i in 1..31 do
+#   if i %3 == 0 && i %5 == 0
+#     print "FizzBuzz,"
+#   elsif i %3 == 0
+#     print "Fizz,"
+#   elsif i %5 == 0
+#     print "Buzz,"
+#   else
+#     print i , ","
+#   end
+# end
+
+# 変数を使用
+# a = "Fizz,"
+# b = "Buzz,"
+# c = "FizzBuzz,"
+# i = 1
+# while i < 32
+#   if i %3 == 0 && i %5 == 0
+#     print c
+#   elsif i %3 == 0
+#     print a
+#   elsif i %5 == 0
+#     print b
+#   else
+#     if i < 31
+#       print i
+#       print ","
+#     else
+#       print i
+#     end
+#   end
+#   i += 1
+# end
+
+# 配列操作
+array = Array.new(100){|i| i+1}
+array.map!{ |i|
+  if i % 3 == 0 && i % 5 == 0
+    "Fizz,Buzz"
+  elsif i % 3 == 0 
+    "Fuzz"
+  elsif i % 5 == 0
+    "Buzz"
   else
-    print i
-    print ","
+    i
   end
-  i += 1
-end
+}
+print array
--- simple-game.rb
+++ simple-game.rb
@@ -1,9 +1,13 @@
 s = "123\n456\n789\n"
 9.times do |i|
   print s
-  print "where?"
+  #順番を決める(iの剰余で順番を判別0か1)
+  j = i%2
+  #oかxかを定義
+  ox = if j == 0 then "o" else "x" end
+  print "#{ox}: where?"
   k = gets.strip
   puts k
-  s.gsub!(/#{k}/, "o")
+  s.gsub!(/#{k}/, ox)
 end
 gets
