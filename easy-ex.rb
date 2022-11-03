93ccec7be003581f24a41c342bde840f7ac67f41
Sumio K <tap.miwo@gmail.com>
2022-11-03 16:47:20 +0900

    comp if

---
+++ easy-ex.rb
@@ -0,0 +1,57 @@
+# no.1
+
+puts "Hello World"
+
+# no.2
+
+name = "Yuki"
+puts "Hello, my name is #{name}"
+
+# no.3
+
+num1 = 3
+num2 = 2
+puts "num1は#{num1},num2は#{num2},合計すると#{num1 + num2}になります。"
+
+# no.4
+
+num1 = 3
+num2 = 2
+puts "num1は#{num1},num2は#{num2},差はすると#{num1 - num2}になります。"
+
+# no.5
+
+num1 = 3
+num2 = 2
+puts "num1は#{num1},num2は#{num2},積は#{num1 * num2}になります。"
+
+# no.6
+
+num1 = 3
+num2 = 2
+puts "num1は#{num1},num2は#{num2},商は#{num1 / num2}になります。"
+
+# no.7
+
+num1 = 3
+num2 = 2
+puts "num1は#{num1},num2は#{num2},num1をnum2で割った余りは#{num1 % num2}になります。"
+
+# no.8
+num1 = 20
+if num1 > 50
+  puts "num1は50よりも大きいです"
+else
+  puts "num1は50以下です"
+end
+
+# no.9
+
+num1 = 20
+if num1 %2 == 0
+  puts "num1は偶数です"
+else
+  puts "num1は奇数です"
+end
+
+# no.10
